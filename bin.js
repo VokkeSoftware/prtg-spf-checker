bc30cc489a7c212e1a783f2dcbf7232d1e3dd71e
Adrian <adrian@vokke.com.au>
2020-06-09 23:35:04 +1000

    Committed v1

---
+++ bin.js
@@ -0,0 +1,11 @@
+const spf = require('spf-check');
+const argv = require('yargs').argv;
+const validator = new spf.SPF(argv.domain);
+
+validator.check(argv.ipAddress).then(result => {
+    if ('Pass' === result.result){
+        console.log('0:SPF check passes for host ' + argv.ipAddress);
+    }else{
+        console.log('2:SPF check fails for host ' + argv.ipAddress);
+    }
+});
